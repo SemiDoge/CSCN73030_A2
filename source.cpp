957dd285139919898359dd769329c54dfbad6df5
SemiDoge <taelforlife@gmail.com>
2024-09-16 19:18:43 +0100

    Add parsing of student txt files

--- source.cpp
+++ source.cpp
@@ -1,3 +1,46 @@
+#include <vector>
+#include <string>
+#include <fstream>
+#include <format>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+struct STUDENT_DATA {
+	std::string firstName;
+	std::string lastName;
+};
+
+STUDENT_DATA processLine(std::string line) {
+	std::string fname, lname;
+
+	std::istringstream line_stringstream(line);
+	line_stringstream >> lname >> fname;
+
+	lname.erase(std::remove(lname.begin(), lname.end(), ','), lname.end());
+
+	STUDENT_DATA newStudent = {.firstName = fname, .lastName = lname};
+	return newStudent;
+}
+
 int main() {
-	return 1;
+	std::vector<STUDENT_DATA> studentData;
+	std::string studentDataFilename = "StudentData.txt";
+
+	std::ifstream ipf(studentDataFilename);
+	if (!ipf.is_open()) {
+		std::cout << std::format("Could not open {}", studentDataFilename);
+	} else {
+		while (!ipf.eof()) {
+			std::string line;
+			std::getline(ipf, line);
+			if (!line.empty()) {
+				studentData.push_back(processLine(line));
+			}
+		}
+	}
+
+	std::cout << std::format("studentData(sz: {}, lastIDX: {}, {})", studentData.size(), studentData[studentData.size()-1].lastName, studentData[studentData.size()-1].firstName);
+
+	return 0;
 }
